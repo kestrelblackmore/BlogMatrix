68b356bdbe993d45b50e8bd9586235b315188e5d
Kestrel Blackmore <me@kestrelblackmore.com>
2013-10-31 21:17:58 +1030

    refactor loading metadata so can load a single json file

--- App_Code/PostMetaData.cs
+++ App_Code/PostMetaData.cs
@@ -8,17 +8,29 @@ using System.IO;
 /// </summary>
 public static class PostMetaData
 {
+
     public static SortedDictionary<string, dynamic> LoadPostMetaData(string pPostMetaDataPath) {
         var files  = new DirectoryInfo(pPostMetaDataPath).GetFiles();
         var result = new SortedDictionary<string, dynamic>(new ReverseComparer<string>(Comparer<string>.Default));
 
         foreach(var file in files) {
-            using (var s = new StreamReader(file.FullName)) {
-                var bits = System.Web.Helpers.Json.Decode(s.ReadToEnd());
+            var bits = GetPostMetaData(file.FullName);
+            
+            if (bits != null)
                 result.Add(bits.publish_date, bits);
-            }
         }
 
         return result;
+    }
+
+    public static dynamic GetPostMetaData(string pFilename) {
+        dynamic metadata = null;
+
+        using (var s = new StreamReader(pFilename)) {
+            metadata = System.Web.Helpers.Json.Decode(s.ReadToEnd());
+        }
+
+        return metadata;
+
     }
 }
